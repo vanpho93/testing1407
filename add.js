d70b5b8dce57d2f496d767cd0d2bf0e34dd0705d
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-12 20:07:32 +0700

    addPromise

--- add.js
+++ add.js
@@ -6,4 +6,14 @@ function addAsync(a, b, cb) {
     setTimeout(() => cb(a + b), 500);
 }
 
+function addPromise(a, b) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const gotTypeError = typeof a !== 'number' || typeof b !== 'number';
+            if(gotTypeError) return reject(new Error('Type error'));
+            resolve(a + b);
+        }, 500);
+    });
+}
+
 module.exports = { add, addAsync };
